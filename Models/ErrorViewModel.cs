933db1c45da6c480b0e77b7beeab8c9d50cfec08
Chris Siddall <Chris.Siddall@cpplc.com>
2021-08-16 14:14:21 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NCReactPortfolioWebsite.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
